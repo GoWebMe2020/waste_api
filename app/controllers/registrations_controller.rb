2a98ba60adf540a9ecb0a37772dd963268af7799
GoWebMe <jpferreira1982@gmail.com>
2021-11-27 11:03:40 +0000

    Fixinf a small error in registrations_controller

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -4,15 +4,15 @@ class RegistrationsController < ApplicationController
     user = User.create!(
       email: params["user"]["email"],
       password: params["user"]["password"],
-      password_confirmation: params["user"]{"password_confirmation"}
+      password_confirmation: params["user"]["password_confirmation"]
     )
 
     if user
-      sessions[:user_id] = user.id
+      session[:user_id] = user.id
       render json: {
         status: :created,
         logged_in: true,
-        message: "You have successfully signed up. Welcome to Waste App."
+        message: "You have successfully signed up. Welcome to Waste App.",
         user: user
       }
     else
